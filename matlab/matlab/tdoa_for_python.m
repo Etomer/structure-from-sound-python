d9e914e27e9aff2eea10805da4a0118e088f2c90
Etomer <erik.tegler@gmail.com>
2023-11-01 09:46:27 +0100

    Added "try catch" so matlab crash not stop system

--- matlab/matlab/tdoa_for_python.m
+++ matlab/matlab/tdoa_for_python.m
@@ -1,25 +1,34 @@
 function tdoa_for_python(folder)
-   file = fullfile(folder,"tdoa_vectors_to_matlab.csv");
 
-    % will be called from the projects main directory so we add path to the
-    % matlab files
-    addpath(genpath("matlab"));
+    try
+       warning('off')
+       file = fullfile(folder,"tdoa_vectors_to_matlab.csv");
     
-    %read data
-    ztmp = csvread(file);
-    
-    % some data prepping
-    ztmp = ztmp(2:end,2:end) + 0.1;
-    ztmp = ztmp';
-    %okcols = find(sum(isfinite(ztmp))>=5);
-    
-    %solving tdoa problem
-    [r, s, o, sol] = tdoa(ztmp, 'display', 'none', 'sigma', 0.02);
-    s = s - mean(r')';
-    r = r - mean(r')';
-    
-    % store results
-    writematrix(s,fullfile(folder,"sender_positions.csv"));
-    writematrix(r,fullfile(folder,"receiver_positions.csv"));
-    writematrix(o,fullfile(folder,"offsets.csv"));
+        % will be called from the projects main directory so we add path to the
+        % matlab files
+        addpath(genpath("matlab"));
+        
+        %read data
+        ztmp = csvread(file);
+        
+        % some data prepping
+        ztmp = ztmp(2:end,2:end) + 0.1;
+        ztmp = ztmp';
+        %okcols = find(sum(isfinite(ztmp))>=5);
+        
+        %solving tdoa problem
+        [r, s, o, sol] = tdoa(ztmp, 'display', 'none', 'sigma', 0.02);
+        s = s - mean(r')';
+        r = r - mean(r')';
+        
+        % store results
+        writematrix(s,fullfile(folder,"sender_positions.csv"));
+        writematrix(r,fullfile(folder,"receiver_positions.csv"));
+        writematrix(o,fullfile(folder,"offsets.csv"));
+
+    catch e
+        disp("Error: ")
+        disp(e.message)
+    end
+    warning('on')
 end
